dbeb22d95730767ccf61eebe2e960c931d1e0dfd
nefir2 <vadim.ahtamov@gmail.com>
2023-05-09 23:13:17 +0300

    some tryings to make this like cpp code

--- c/thc.c
+++ c/thc.c
@@ -1,10 +1,20 @@
 #include "stdio.h"
-#include "windows.h"
+#include "stdlib.h"
 
 int main(int argc, char **argv) {
-	int argl = argc - 1; //args without path to this program
-	if (argl == 2) system(sprintf(".\\thcrap_loader.exe th%d %s", argv[1], argv[2]));
-	else if (argl == 1 && argv[1] ) system(sprintf(".\\thcrap_loader.exe th%d ru.js", argv[1]));
-	else if (argl == 0) printf("input a number of touhou game.");
+	int argl = argc - 1; //args length. args without path to this program
+	char *launch;
+	if (argl == 2) {
+		snprintf(launch, 40 * sizeof(char), ".\\thcrap_loader.exe th%s %s", argv[1], argv[2]);
+		system(launch);
+	}
+	else if (argl == 1 && argv[1] ) {
+		snprintf(launch, 40 * sizeof(char), ".\\thcrap_loader.exe th%s ru.js", argv[1]);
+		system(launch);
+	}
+	else if (argl <= 0 || argl >= 3 ) {
+		printf("write a number of touhou game.");
+		return 0;
+	}
 	return 0;
 }

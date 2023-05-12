b131df4d7f2d23289ed3a15a7971fc1ac0514cfd
nefir2 <vadim.ahtamov@gmail.com>
2023-05-13 02:53:06 +0300

    some tryings to make on csharp

---
+++ csharp/thc/Program.cs
@@ -0,0 +1,53 @@
+﻿using System; //cw
+using System.Reflection; //assembly
+
+namespace thc
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			bool isExThc = thc.CheckPath("thcrap_loader.exe", out bool isl);
+            Console.WriteLine
+			(
+				$"thcrap_loader is found: {isExThc}\n" +
+				$"thcrap in . folder: {isl}"
+			);
+            switch (args.Length) //thcrap_loader.exe
+			{
+				case 2:
+
+					break;
+				case 1:
+					if (args[0].StartsWith("-h") || args[0].StartsWith("--h"))
+					{
+						Usage();
+						return;
+					}
+					break;
+				default:
+					Usage();
+					return;
+			}
+		}
+		/// <summary>
+		/// shows manual for this program.
+		/// </summary>
+		public static void Usage()
+		{
+			Console.WriteLine
+			(
+				$"Usage: {Assembly.GetExecutingAssembly().Location} {{th}} [lang]\n" +
+				$"\topening touhou game with chosen number and default language.\n" +
+				$"\tth - number of touhou game. may be like \"th06\" or like \"06\".\n" +
+				$"\tlang - language for game from config json files (default=ru.js)\n"
+			);
+		}
+		/// <summary>
+		/// checking path for <paramref name="file"/> in this folder or in %PATH% Variable.
+		/// </summary>
+		/// <param name="file">file to check</param>
+		/// <param name="isLocal">if file in local folder returns <see langword="true"/></param>
+		/// <returns></returns>
+	}
+}
---
+++ csharp/thc/thc.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics; //process
+using System.IO; //path, file
+using System.Text; //stringbuilder
+
+namespace thc
+{
+	public static class thc
+	{
+		public static string CutToFirst(this string str, char symbol, out int firstSpace)
+		{
+			string ret = "";
+			StringBuilder strbld = new StringBuilder(str);
+			firstSpace = -1;
+			for (int i = 0; i < str.Length - 1; i++)
+			{
+				if (str[i + 1] != symbol) ret += str[i];
+				else
+				{
+					firstSpace = i + 1;
+					break;
+				}
+			}
+			return ret;
+		}
+		public static bool CheckPath(string file, out bool isLocal) 
+		{
+			isLocal = File.Exists($".\\{file}");
+			return File.Exists($"{file}") || isLocal;
+			//return overall || local ? (true, overall) : (overall, local);
+		}
+		public static void Launch(string fileAndArgs)
+		{
+			string file = fileAndArgs.CutToFirst(' ', out int i);
+			string args = fileAndArgs.Substring(i + 1);
+			Process.Start(file, args);
+		}
+	}
+}

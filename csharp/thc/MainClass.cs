f8112dbd45f0d72a2a86c28732cdfa87e15fd0c8
nefir2 <vadim.ahtamov@gmail.com>
2023-05-23 23:39:45 +0300

    less constants

--- csharp/thc/MainClass.cs
+++ csharp/thc/MainClass.cs
@@ -33,6 +33,34 @@ namespace thc
 		/// </summary>
 		static readonly string jsonPath;
 		/// <summary>
+		/// name of config folder in thcrap folder.
+		/// </summary>
+		static readonly string configFolderName;
+		/// <summary>
+		/// name of folder bin in thcrap folder.
+		/// </summary>
+		static readonly string binName;
+		/// <summary>
+		/// name of program for configuration thcrap (old version) in thcrap/bin folder.
+		/// </summary>
+		static readonly string configureName;
+		/// <summary>
+		/// name of program for configuration thcrap (new version) in thcrap/bin folder.
+		/// </summary>
+		static readonly string configureV3Name;
+		/// <summary>
+		/// name of program that testing dependecies in thcrap/bin folder.
+		/// </summary>
+		static readonly string testName;
+		/// <summary>
+		/// name of program that help edit json file when it need to be repaired.
+		/// </summary>
+		static readonly string editor;
+		/// <summary>
+		/// array of js(on) files that is not a languages.
+		/// </summary>
+		static readonly string[] notLangNames;
+		/// <summary>
 		/// instance of <see cref="ThSettings"/> for saving in json file.
 		/// </summary>
 		static ThSettings settings;
@@ -40,9 +68,17 @@ namespace thc
 		#region ctors
 		static MainClass()
 		{
-			point = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			editor = "notepad";
+			binName = "bin";
+			testName = "thcrap_test.exe";
 			thcrapload = "thcrap_loader.exe";
+			notLangNames = new string[] { "config.js", "games.js", "mods.js" };
+			configureName = "thcrap_configure.exe";
+			configureV3Name = "thcrap_configure_v3.exe";
+			configFolderName = "config";
 			settingsFolderName = "thc config";
+
+			point = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 			settingsFolderPath = Path.Combine(point, settingsFolderName);
 			jsonPath = $@"{settingsFolderPath}\{Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location)}.json";
 
@@ -94,13 +130,13 @@ namespace thc
 						switch (args[0])
 						{
 							case "--configure":
-								Thc.Launch($"{settings.ThcrapPath}\\bin\\thcrap_configure_v3.exe");
+								Thc.Launch(Path.Combine(settings.ThcrapPath, binName, configureV3Name));
 								return;
 							case "--test":
-								Thc.Launch($"{settings.ThcrapPath}\\bin\\thcrap_test.exe");
+								Thc.Launch(Path.Combine(settings.ThcrapPath, binName, testName));
 								return;
 							case "--repair":
-								Thc.Launch($"notepad {jsonPath}");
+								Thc.Launch($"{editor} {jsonPath}");
 								return;
 							default:
 								Usage();
@@ -121,19 +157,19 @@ namespace thc
 								JsonSaver.MakeFile(jsonPath, settings);
 								return;
 							case "--thcrap":
-								if (!Directory.Exists(args[1])) Console.WriteLine("folder not exist or path is not a folder.");
-								else
+								if (Directory.Exists(args[1]))
 								{
-									if (!File.Exists(Path.Combine(args[1], thcrapload)) && !Directory.Exists(Path.Combine(args[1], "config"))) Console.WriteLine("not a thcrap folder.");
-									else
+									if (File.Exists(Path.Combine(args[1], thcrapload)) && Directory.Exists(Path.Combine(args[1], configFolderName)))
 									{
 										settings.ThcrapPath = args[1];
 										JsonSaver.MakeFile(jsonPath, settings);
 									}
+									else Console.WriteLine("not a thcrap folder.");
 								}
+								else Console.WriteLine("folder not exist or path is not a folder.");
 								return;
 							case "--configure":
-								if (args[1].Equals("-o") || args[1].Equals("-old")) Thc.Launch($"{settings.ThcrapPath}\\bin\\thcrap_configure.exe");
+								if (args[1].Equals("-o") || args[1].Equals("-old")) Thc.Launch(Path.Combine(settings.ThcrapPath, binName, configureName));
 								else Usage();
 								return;
 							case "--repair":
@@ -171,16 +207,12 @@ namespace thc
 						case "-l":
 						case "-langs":
 						case "-languages":
-							string[] langs = Directory.GetFiles(Path.Combine(settings.ThcrapPath, "config"));
+							string[] langs = Directory.GetFiles(Path.Combine(settings.ThcrapPath, configFolderName));
 							Console.WriteLine("languages:");
 							for (int i = 0; i < langs.Length; i++)
 							{
-								if
-								(
-									Path.GetFileName(langs[i]) != "config.js" &&
-									Path.GetFileName(langs[i]) != "games.js" &&
-									Path.GetFileName(langs[i]) != "mods.js"
-								) Console.WriteLine($"\t{Path.GetFileName(langs[i])}");
+								foreach (string notLang in notLangNames) if (Path.GetFileName(langs[i]) == notLang) return;
+								Console.WriteLine($"\t{Path.GetFileName(langs[i])}");
 							}
 							return;
 						case "-h":
@@ -240,8 +272,8 @@ namespace thc
 				"\t--th {num} - set number of default touhou.\n"														+
 				"\t--lang[uage] {str} - set default language file.\n"													+
 				"\t--thcrap {path} - set folder where can be found thcrap_loader.exe\n"									+
-				"\t--test - launch thcrap_test.\n"																		+
-				"\t--configure [-o[ld]] - launch thcrap_configure.\n"													+
+				"\t--test - launch thcrap_test.exe\n"																	+
+				"\t--configure [-o[ld]] - launch thcrap_configure.exe\n"												+
 				"\t\t-o[ld] - launch old version.\n"																	+
 				"\t--repair [-d[efault]|-e[mpty]] - repair json file if it broken.\n"									+
 				"\t\t-d[efault] - returns default settings.\n" +
